609fcff6fb968c9fa272b3e20c53c7fddce6d107
Samira Karimi <samira.ka006@gmail.com>
2019-06-11 15:17:47 -0500

    change const format

--- 03-props-and-more/03-slots-exercise/Machine.js
+++ 03-props-and-more/03-slots-exercise/Machine.js
@@ -1,8 +1,6 @@
 class Machine extends React.Component {
   render() {
-    const n1 = this.props.n1;
-    const n2 = this.props.n2;
-    const n3 = this.props.n3;
+    const { n1, n2, n3 } = this.props;
 
     return (
       <div>
