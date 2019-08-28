aa14e45df60db083b94fe44ab6ab04feff8be382
Samira Karimi <39065116+SamiraKarimi@users.noreply.github.com>
2019-08-28 11:22:42 -0500

    Update Machine.js

--- 03-props-and-more/03-slots-exercise/Machine.js
+++ 03-props-and-more/03-slots-exercise/Machine.js
@@ -1,13 +1,13 @@
 class Machine extends React.Component {
   render() {
     const { n1, n2, n3 } = this.props;
+    const winner = (n1 === n2 && n2 === n3);
 
     return (
       <div>
-        <p>
-          {n1} {n2} {n3}
-        </p>
-        {n1 === n2 && n1 === n3 ? "You win!" : "You lose!"}
+        <p>{n1}{n2}{n3}</p>
+       <p>{winner ? 'winner' : 'looser'}</p>
+       <br/>
       </div>
     );
   }
