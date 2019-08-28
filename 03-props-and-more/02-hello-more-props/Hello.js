0a05c544d6650ac599adee4f566bf3dccaeb6aa3
Samira Karimi <39065116+SamiraKarimi@users.noreply.github.com>
2019-08-28 09:46:45 -0500

    Update Hello.js

--- 03-props-and-more/02-hello-more-props/Hello.js
+++ 03-props-and-more/02-hello-more-props/Hello.js
@@ -4,7 +4,7 @@ class Hello extends React.Component {
     return (
       <div>
         <p>
-          Hi {this.props.to} from {this.props.from} {bangs}
+          Hi {this.props.to} from {this.props.from}{bangs}
         </p>
         <img src={this.props.img} />
       </div>
